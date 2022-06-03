c61cc61dddc8d4e4d5e1c670bc9c3b924ae9ea97
PicusMaximus <jan.lennart.stuetten@gmail.com>
2022-06-03 08:50:54 +0200

    fixed ressource disposing

--- Schulaufgaben/src/WriteFile.java
+++ Schulaufgaben/src/WriteFile.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WriteFile {
@@ -7,6 +8,7 @@ public class WriteFile {
     public static void main(String[] args) throws IOException {
         String filePath = "Schulaufgaben/Data/Classes";
         File folder = new File(filePath);
+        ArrayList<String> names = new ArrayList<>();
         ArrayList<String> classes = new ArrayList<>();
         File[] listOfFiles = folder.listFiles();
         assert listOfFiles != null;
@@ -16,39 +18,46 @@ public class WriteFile {
                 classes.add(folder + File.separator + file.getName());
             }
         }
-        BufferedReader br = new BufferedReader(new FileReader(classes.get(0)));
-        ArrayList<String> names = new ArrayList<>();
-        String name = null;
-        for(int i = 0; i< 3;i++){
-            name = br.readLine();
-        }
-        while(name != null){
-            name = name.split(";")[0];
-            names.add(name);
-            name = br.readLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(classes.get(0)))){
+
+            String name = null;
+            for(int i = 0; i< 3;i++){
+                name = br.readLine();
+            }
+            while(name != null){
+                name = name.split(";")[0];
+                names.add(name);
+                name = br.readLine();
+            }
         }
 
-        for(String n: names){
-            File output = new File("Resources" + File.separator + "Students");
-            if(!output.exists()){
-                output.mkdirs();
-            }
-            BufferedWriter bw = new BufferedWriter(new FileWriter(output + File.separator + n + ".txt"));
-            bw.write("Zeugnis für " + n);
-            for(String c:classes){
-                bw.newLine();
-                br = new BufferedReader(new FileReader(c));
-                String fach = br.readLine().split(";")[1];
-                String note = null;
-                for(int i = 0; i< 2 + names.indexOf(n);i++){
-                    note = br.readLine();
+
+        boolean result = true;
+        File output = new File("Resources" + File.separator + "Students");
+        if(!output.exists()){
+            result = output.mkdirs();
+        }
+        if(result){
+            for(String n: names){
+
+                try(BufferedWriter bw = new BufferedWriter(new FileWriter(output + File.separator + n + ".txt"))){
+                    bw.write("Zeugnis für " + n);
+                    for(String c:classes){
+                        bw.newLine();
+                        try(BufferedReader br = new BufferedReader(new FileReader(c))){
+                            String fach = br.readLine().split(";")[1];
+                            String note = null;
+                            for(int i = 0; i< 2 + names.indexOf(n);i++){
+                                note = br.readLine();
+                            }
+                            assert note != null;
+                            note = note.split(";")[1];
+                            bw.write(fach + ": " + note);
+                        }
+                    }
                 }
-                assert note != null;
-                note = note.split(";")[1];
-                bw.write(fach + ": " + note);
             }
-            bw.close();
-            br.close();
         }
     }
 }
